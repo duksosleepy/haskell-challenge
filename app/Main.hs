a68df6086fb028eb010f97ebde86bfafac72d8db
Song Khoi <songkhoi123@gmail.com>
2025-02-18 01:23:12 +0700

    add atom and string

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,43 @@
-module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
